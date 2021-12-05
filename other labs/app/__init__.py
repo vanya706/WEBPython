06c31e6bcacc94c8cf0a2468ee4678c55d32b812
Ivan Mostovyi <vanya.mostoviy@gmail.com>
2021-12-05 22:47:56 +0200

    Implemented lab 9

--- other labs/app/__init__.py
+++ other labs/app/__init__.py
@@ -2,11 +2,15 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_migrate import Migrate
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config.from_object('config')
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'warning'
 
 bcrypt = Bcrypt(app)
-from . import view, forms, function
+from . import view, models
