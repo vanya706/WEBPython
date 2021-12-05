a777dc947e2922246b33290a1b437daabc99d98b
Ivan Mostovyi <vanya.mostoviy@gmail.com>
2021-12-05 18:18:33 +0200

    Implemented lab 7

--- other labs/app/forms.py
+++ other labs/app/forms.py
@@ -1,6 +1,8 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, TextAreaField, SelectField, PasswordField
-from wtforms.validators import DataRequired, Length, Email, InputRequired, EqualTo, Regexp
+from wtforms.validators import DataRequired, Length, Email, InputRequired, EqualTo, Regexp, ValidationError
+
+from .models import User
 
 
 class ContactForm(FlaskForm):
@@ -52,4 +54,41 @@ class DataForm(FlaskForm):
                           ('2016', '2016'), ('2015', '2015'), ('2014', '2014'), ('2013', '2013')])
     serial = StringField('Serial number')
     number_doc = StringField('Number document*')
-    submit = SubmitField(label=('Submit'))
+    submit = SubmitField(label='Submit')
+
+
+class SignUpForm(FlaskForm):
+    username = StringField('Username',
+                           validators=[InputRequired('A username is required'),
+                                       Length(min=4, max=14,
+                                              message='Name must have greater 4 symbol and least 14 symbol'),
+                                       Regexp('^[A-Za-z][A-Za-z0-9_.]*$', 0,
+                                              'Username must have only letters, numbers, dots or underscores')])
+    email = StringField('Email',
+                        validators=[InputRequired('Email is required'),
+
+                                    Regexp('^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$',
+                                           message='Invali Email')])
+    password1 = PasswordField('Password',
+                              validators=[InputRequired('Password is required'),
+                                          Length(min=6, message='Must be at least 6')])
+    password2 = PasswordField('Repeat the password',
+                              validators=[InputRequired('Password is required'),
+                                          Length(min=6, message='Must be at least 6'),
+                                          EqualTo('password1')])
+    submit = SubmitField(label='')
+
+    def validate_email(self, field):
+        if User.query.filter_by(email=field.data).first():
+            raise ValidationError('Email already registered')
+
+    def validate_username(self, field):
+        if User.query.filter_by(username=field.data).first():
+            raise ValidationError('Username already registered')
+
+
+class LoginForm(FlaskForm):
+    email = StringField('Email',
+                        validators=[DataRequired(), Regexp('^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$')])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField(label='')
