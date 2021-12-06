b3455ed2c1416dc52fd88f3aaf024a974468cef0
Ivan Mostovyi <vanya.mostoviy@gmail.com>
2021-12-06 06:56:17 +0200

    Implemented lab 12

--- other labs/app/auth/forms.py
+++ other labs/app/auth/forms.py
@@ -1,8 +1,11 @@
+from flask_login import current_user
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from flask_wtf.file import FileField, FileAllowed
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextAreaField
 from wtforms.validators import InputRequired, Length, Regexp, EqualTo, DataRequired, ValidationError
 
 from .models import User
+from .. import bcrypt
 
 
 class SignUpForm(FlaskForm):
@@ -35,3 +38,40 @@ class LoginForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired()])
     remember = BooleanField('')
     submit = SubmitField(label=(''))
+
+
+class UpdateAccountForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
+    email = StringField('Email', validators=[InputRequired('Email is required'),
+                                             Regexp('^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$',
+                                                    message='Invali Email')])
+    picture = FileField('Update Profile Picture', validators=[FileAllowed(['jpg', 'png'])])
+    about_me = TextAreaField('About Me', validators=[DataRequired(), Length(max=500)])
+    submit = SubmitField('')
+
+    def validate_email(self, field):
+        if field.data != current_user.email:
+            if User.query.filter_by(email=field.data).first():
+                raise ValidationError('Email already registered')
+
+    def validate_username(self, field):
+        if field.data != current_user.username:
+            if User.query.filter_by(username=field.data).first():
+                raise ValidationError('Username already registered')
+
+
+class ResetPasswordForm(FlaskForm):
+    old_password = PasswordField('Old Password', validators=[DataRequired()])
+
+    new_password1 = PasswordField('Password',
+                                  validators=[InputRequired('Password is required'),
+                                              Length(min=6, message='Must be at least 6')])
+
+    new_password2 = PasswordField('Repeat the password',
+                                  validators=[InputRequired('Password is required'),
+                                              Length(min=6, message='Must be at least 6'),
+                                              EqualTo('new_password1')])
+
+    def validate_old_password(self, field):
+        if not bcrypt.check_password_hash(current_user.password, field.data):
+            raise ValidationError('Error')
