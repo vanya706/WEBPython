574d479373f658d3fb9352e5c4f7dac9b44d918d
Ivan Mostovyi <vanya.mostoviy@gmail.com>
2022-01-11 05:37:37 +0200

    Trying to fix heroku deployment

--- run.py
+++ run.py
@@ -1,4 +1,9 @@
 other_labs = __import__("other labs.app")
 
+
+def create_app():
+    return other_labs.app.create_app(config_name='dev')
+
+
 if __name__ == '__main__':
-    other_labs.app.create_app(config_name='dev').run()
+    create_app().run()
